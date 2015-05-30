cfac30ae3f4b217e87b9a7760e12dff936e1ad83
mlmedia <mike@millionlightsmedia.com>
2015-05-29 21:58:44 -0400

    - adding markup + separate css + js

--- js/accordion.js
+++ js/accordion.js
@@ -4,49 +4,49 @@
 /* define $ as jQuery just in case */
 ( function( $ )
 {
-	/* toggle / accordian events - my custom plugin */
-	$.fn.accordian = function( speed ) 
+	/* toggle / accordion events - my custom plugin */
+	$.fn.accordion = function( speed )
 	{
-
 		/* set vars */
-		var accordian 		= this;
-		var groups 			= accordian.find( '.group' );
-		var boxes 			= accordian.find( '.box' );
-		var controls 		= accordian.find( '.control' );		
+		var accordion 		= this;
+		var groups 			= accordion.find( '.group' );
+		var boxes 			= accordion.find( '.box' );
+		var controls 		= accordion.find( '.control' );
 		var active_controls = controls.find( '.current' );
-		var active_control 	= active_controls.first( ).length > 0 ? active_controls.first( ) : accordian.find( '.control:eq(0)' );
+		var active_control 	= active_controls.first( ).length > 0 ? active_controls.first( ) : accordion.find( '.control:eq(0)' );
 		var speed = ( ! speed ) ? 300 : speed;
-		
-		/** 
-		 * slide up all of the toggle targets and open the ones with the parent toggle box set to open - very fast so as to not be visible 
+
+		/**
+		 * slide up all of the toggle targets and open the ones with the parent toggle box set to open - very fast so as to not be visible
 		 * - we do this to eliminate the hitch in the animation by animating up first instead of using "display:none;"
-		 * - before sliding up, grab the collapsed accordian height plus one expanded box and set as min-height to get rid of Chrome animation choppiness
+		 * - before sliding up, grab the collapsed accordion height plus one expanded box and set as min-height to get rid of Chrome animation choppiness
 		 */
-		var exp_height = accordian.height( );
-		accordian.hide( );
+		var exp_height = accordion.height( );
+		accordion.hide( );
 		boxes.slideUp( 0 );
-		
-		/* on page load - show the accordian */
-		$( window ).load( function( ) {
-		
-			/* show the accordian when everything is loaded */
-			accordian.show( )
-			var coll_height = accordian.height( );
-			accordian.css({ 
+
+		/* on page load - show the accordion */
+		$( window ).load( function( )
+		{
+			/* show the accordion when everything is loaded */
+			accordion.show( )
+			var coll_height = accordion.height( );
+			accordion.css({
 				'min-height':parseInt( coll_height + exp_height / boxes.length )
 			});
-			
+
 		});
-		
-		/* accordian control click action */
-		accordian.on( 'click', '.control', function( e ) {
+
+		/* accordion control click action */
+		accordion.on( 'click', '.control', function( e )
+		{
 			var parent_el 	= $( this ).closest( '.group' );
 			var target 		= parent_el.find( '.box' );
 			if ( parent_el.hasClass( 'open' ) )
 			{
 				parent_el.removeClass( 'open' );
 				target.slideUp( speed );
-			} 
+			}
 			else
 			{
 				groups.removeClass( 'open' );
@@ -56,7 +56,7 @@
 			}
 			e.preventDefault( );
 		});
-		
+
 	}
-	
+
 })( jQuery );
