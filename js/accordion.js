fe7e52fc589dd9b06c6b22fee454c048b704ef75
mlmedia <millionlightsmedia@gmail.com>
2015-06-05 22:16:42 -0400

    - animation option + fixing choppiness + more html

--- js/accordion.js
+++ js/accordion.js
@@ -2,67 +2,58 @@
  * jQuery plugin for an accordion
  */
 /* define $ as jQuery just in case */
-( function( $ )
+(function($)
 {
 	/* toggle / accordion events - my custom plugin */
-	$.fn.accordion = function( options )
+	$.fn.accordion = function(options)
 	{
 		/* set the default options */
 		var settings = $.extend({
-            speed: 300,
-            multiple: false
-        }, options );
+			animation: false,
+			speed: 200,
+			multiple: false
+		}, options);
 
 		/* set vars */
-		var accordion 		= this;
-		var groups 			= accordion.find( '.group' );
-		var boxes 			= accordion.find( '.box' );
-		var controls 		= accordion.find( '.control' );
-		var active_controls = controls.find( '.current' );
-		var active_control 	= active_controls.first( ).length > 0 ? active_controls.first( ) : accordion.find( '.control:eq(0)' );
+		var accordion = this;
+		var groups = accordion.find('.group');
+		var boxes = accordion.find('.box');
+		var controls = accordion.find('.control');
+		var active_controls = controls.find('.current');
+		var active_control = active_controls.first().length > 0 ? active_controls.first() : accordion.find('.control:eq(0)');
 
-		/**
-		 * slide up all of the toggle targets and open the ones with the parent toggle box set to open - very fast so as to not be visible
-		 * - we do this to eliminate the hitch in the animation by animating up first instead of using "display:none;"
-		 * - before sliding up, grab the collapsed accordion height plus one expanded box and set as min-height to get rid of Chrome animation choppiness
-		 */
-		var exp_height = accordion.height( );
-		accordion.hide( );
-		boxes.slideUp( 0 );
+		/* hide the accordion and close all the boxes */
+		accordion.hide();
+		boxes.hide();
 
 		/* on page load - show the accordion */
-		$( window ).load( function( )
+		$( window ).load( function()
 		{
-			/* show the accordion when everything is loaded */
-			accordion.show( )
-			var coll_height = accordion.height( );
-			/*
-			accordion.css({
-				'min-height':parseInt( coll_height + exp_height / boxes.length )
-			});
-			*/
-
 			/* hide the collapse icon, unless the group is marked open, then hide the expander icon */
 			$.each( groups, function(i, group)
 			{
 				if ($(groups[i]).hasClass('open'))
 				{
 					$(groups[i]).find('.expand').hide();
+					$(groups[i]).find('.box').show();
 				}
 				else
 				{
 					$(groups[i]).find('.collapse').hide();
+					$(groups[i]).find('.box').hide();
 				}
 			});
 
+			/* show the accordion when everything is loaded */
+			accordion.show();
 		});
 
 		/* accordion control click action */
-		accordion.on( 'click', '.control', function( e )
+		accordion.on('click', '.control', function(e)
 		{
-			var parent_el 	= $( this ).closest( '.group' );
-			var target 		= parent_el.find( '.box' );
-			if ( parent_el.hasClass( 'open' ) )
+			var parent_el = $( this ).closest('.group');
+			var target = parent_el.find('.box');
+			if ( parent_el.hasClass('open') )
 			{
 				/* hide all the collapse icons if multiple settings is set to false (only one box open at a time) */
 				if (settings.multiple == false)
@@ -71,10 +62,19 @@
 				}
 
 				/* if open, collapse it */
-				parent_el.removeClass( 'open' );
-				parent_el.find( '.collapse' ).hide(); /* hide the collapse icon */
-				parent_el.find( '.expand' ).show(); /* show the expand icon */
-				target.slideUp( settings.speed );
+				parent_el.removeClass('open');
+				parent_el.find('.collapse').hide(); /* hide the collapse icon */
+				parent_el.find('.expand').show(); /* show the expand icon */
+
+				/* if animation is set to true, animate; otherwise show / hide instantly instead */
+				if (settings.animation == true)
+				{
+					target.slideUp(settings.speed);
+				}
+				else
+				{
+					target.hide();
+				}
 			}
 			else /* otherwise, expand it */
 			{
@@ -82,20 +82,37 @@
 				if (settings.multiple == false)
 				{
 					/* close all boxes and hide collapse icons / show all expand icons - before expanding this control / box */
-					groups.removeClass( 'open' );
-					boxes.slideUp( settings.speed );
+					groups.removeClass('open');
 					groups.find('.collapse').hide();
-					groups.find( '.expand' ).show();
+					groups.find('.expand').show();
+
+					/* if animation is set to true, animate; otherwise show / hide instantly instead */
+					if (settings.animation == true)
+					{
+						boxes.slideUp(settings.speed);
+					}
+					else
+					{
+						boxes.hide();
+					}
 				}
 
 				/* open this box */
-				parent_el.addClass( 'open' );
-				parent_el.find( '.expand' ).hide(); /* hide the expand icon */
-				parent_el.find( '.collapse' ).show(); /* show the collapse icon */
-				target.slideDown( settings.speed );
+				parent_el.addClass('open');
+				parent_el.find('.expand').hide(); /* hide the expand icon */
+				parent_el.find('.collapse').show(); /* show the collapse icon */
+
+				/* if animation is set to true, animate; otherwise show / hide instantly instead */
+				if (settings.animation == true)
+				{
+					target.slideDown(settings.speed);
+				}
+				else
+				{
+					target.show();
+				}
 			}
-			e.preventDefault( );
+			e.preventDefault();
 		});
 	}
-
-})( jQuery );
+})(jQuery);
